5579aa15efeb4267472fb6a197506ee5038ce4d7
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-23 12:58:29 -0500

    h container

--- lib/pages/desafios_container_page.dart
+++ lib/pages/desafios_container_page.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DesafiosContainerPage extends StatelessWidget {
+  Widget hContainer() {
+    return Container(
+      width: 280,
+      height: 280,
+      alignment: Alignment.center,
+      // padding: EdgeInsets.all(40),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.orange,
+          width: 10,
+        ),
+      ),
+      child: Text(
+        "H",
+        style: TextStyle(fontSize: 160, color: Colors.orange),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +29,9 @@ class DesafiosContainerPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: [],
+          children: [
+            hContainer(),
+          ],
         ),
       ),
     );
