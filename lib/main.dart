58534a56d3733d1ac0b9d889e92d9dcfa17feab7
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-23 12:50:10 -0500

    desafios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:futuresg10/pages/desafios_container_page.dart';
 import 'package:futuresg10/pages/futurebuilder_page.dart';
 import 'package:futuresg10/pages/home_page.dart';
 import 'package:futuresg10/pages/perfil_page.dart';
@@ -6,7 +7,7 @@ import 'package:futuresg10/pages/perfil_page.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: PerfilPage(),
+      home: DesafiosContainerPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
---
+++ lib/pages/desafios_container_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DesafiosContainerPage extends StatelessWidget {
+  const DesafiosContainerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Containers desafios"),
+      ),
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
