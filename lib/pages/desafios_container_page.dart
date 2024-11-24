efbcef5e6fbdfd7243776e8470993bdf3c2987d4
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-24 09:36:37 -0500

    challenge

--- lib/pages/desafios_container_page.dart
+++ lib/pages/desafios_container_page.dart
@@ -21,6 +21,62 @@ class DesafiosContainerPage extends StatelessWidget {
     );
   }
 
+  Widget iAmHeader() {
+    return Container(
+      height: 130,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.blue.shade200, offset: Offset(9, 9), blurRadius: 6),
+        ],
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "I am Header",
+        style: TextStyle(color: Colors.white, fontSize: 38),
+      ),
+    );
+  }
+
+  Widget barra() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Container(
+        width: 300,
+        height: 90,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(45),
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            height: 90,
+            width: 210,
+            decoration: BoxDecoration(
+              color: Colors.blue.shade200,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(45),
+                bottomLeft: Radius.circular(45),
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              "Challenge",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +87,8 @@ class DesafiosContainerPage extends StatelessWidget {
         child: Column(
           children: [
             hContainer(),
+            iAmHeader(),
+            barra(),
           ],
         ),
       ),
